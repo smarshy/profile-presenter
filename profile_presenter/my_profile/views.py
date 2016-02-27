c8b1868d04408d49c316ee461e3108c0e3a610b1
vatsala <vatsala.swaroop@gmail.com>
2016-02-28 05:26:44 +0530

    Added functions for fetching API data

--- profile_presenter/my_profile/views.py
+++ profile_presenter/my_profile/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from profile_presenter.git_api_interactions import *
+
 def index(request):
     return render_to_response('my_profile/my_profile.html', context_instance=RequestContext(request))
 
---
+++ profile_presenter/profile_presenter/git_api_interactions.py
@@ -0,0 +1,59 @@
+import requests
+from collections import defaultdict
+
+def get_count(url):
+
+	r = requests.get(url)
+	response = r.json()
+	return response['total_count']
+
+def get_items(url):
+
+	r = requests.get(url)
+	response = r.json()
+	return response['items']
+
+def get_contributions_analysis(username):
+	url = "https://api.github.com/search/issues?q=author:" + username + "+type:"
+
+	url1 = url + "issue"
+	url2 = url + "issue+is:closed"
+	url3 = url + "issue+is:open"
+	url4 = url + "pr"
+	url5 = url + "pr+is:open"
+	url6 = url + "pr+is:closed+is:unmerged"
+	url7 = url + "pr+is:closed+is:merged"
+
+	try:
+		statistics['Total Issues'] = get_count(url1)
+		statistics['Closed Issues'] = get_count(url2)
+		statistics['Open Issues'] = get_count(url3)
+		statistics['Total PRs'] = get_count(url4)
+		statistics['Open PRs'] = get_count(url5)
+		statistics['Unmerged PRs'] = get_count(url6)
+		statistics['Merged PRs'] = get_count(url7)		
+	except:
+		print "Check if username is valid/internet connection"
+		return False
+
+	return statistics
+
+def get_language_analysis(username):
+
+	url = "https://api.github.com/search/repositories?q=fork:false+user:" + username 
+	try:
+		repos = get_items(url)
+	except:
+		print "Check if username is valid/internet connection"
+		return False
+
+	for repo in repos:
+		if repo['language']:
+			languages.append(repo['language'])
+
+	analysed = defaultdict(int)
+
+	for language in languages:
+		analysed[language] += 1
+
+	return analysed.items()
