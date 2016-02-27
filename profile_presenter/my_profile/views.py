366cafa6a36a4f226a2393a65be7f36410bcda7e
vatsala <vatsala.swaroop@gmail.com>
2016-02-27 22:38:44 +0530

    Basic working example using google charts

--- profile_presenter/my_profile/views.py
+++ profile_presenter/my_profile/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def index(request):
-	return render_to_response('my_profile/my_profile.html', context_instance=RequestContext(request))
+    return render_to_response('my_profile/my_profile.html', context_instance=RequestContext(request))
+
