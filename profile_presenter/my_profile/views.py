e4379312e49d9605502e556ad20ab8d17efb7e6f
vatsala <vatsala.swaroop@gmail.com>
2016-02-27 19:39:51 +0530

    Adding statis files and basic working template configuration

--- profile_presenter/my_profile/views.py
+++ profile_presenter/my_profile/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
-# Create your views here.
+def index(request):
+	return render_to_response('my_profile/my_profile.html', context_instance=RequestContext(request))
--- profile_presenter/profile_presenter/urls.py
+++ profile_presenter/profile_presenter/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'profile_presenter.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^smarshy/', include ('my_profile.urls', namespace='my_profile')),
+    url(r'^analyse/', include ('general_analysis.urls', namespace='general_analysis')),
+    url(r'^$',views.home,name='home'),
 ]
