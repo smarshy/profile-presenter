7fc315c656c2afefbb5acd948e2f16086dbe4707
vatsala <vatsala.swaroop@gmail.com>
2016-02-28 21:13:28 +0530

    Linking list to template and error check

--- profile_presenter/my_profile/views.py
+++ profile_presenter/my_profile/views.py
@@ -10,13 +10,14 @@ def index(request):
 	languages = get_language_analysis(username)
 	stats = get_contributions_analysis(username)
 	commit_act = get_commit_analysis(username)
-	print get_contributions_list(username)
+	c_list = get_contributions_list(username)
 	print stats
 	print languages
 	print commit_act
+	print c_list
 
-	if languages and stats and commit_act:
-		return render(request, 'my_profile/my_profile.html', {'stats': stats, 'user': username, 'languages': languages, 'commit': commit_act})
+	if languages and stats and commit_act and c_list:
+		return render(request, 'my_profile/my_profile.html', {'stats': stats, 'user': username, 'languages': languages, 'commit': commit_act, 'contributions_list': c_list})
 	else:
 		return render_to_response('my_profile/error_page.html', context_instance=RequestContext(request))
 
